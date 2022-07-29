39b60b4ae9dabf79aabad86b0d18dc6d490b366a
Thang Nguyen <quocthangn1990@gmail.com>
2022-07-29 17:35:55 +0700

    hand & gesture tracking
    -Python 3.9.13

---
+++ HandTrackingModule.py
@@ -0,0 +1,69 @@
+import cv2
+import mediapipe as mp
+
+
+class handDetector():
+    def __init__(self, mode=False, maxHands=2, detectCon=0.5, trackCon=0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectCon = detectCon
+        self.trackCon = trackCon
+
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(static_image_mode=self.mode, max_num_hands=self.maxHands,
+                                        min_detection_confidence=self.detectCon,
+                                        min_tracking_confidence=self.trackCon)
+        self.mpDraw = mp.solutions.drawing_utils
+        self.mpDrawingStyle = mp.solutions.drawing_styles
+        self.tipIds = [4, 8, 12, 16, 20]
+
+        # Video capture
+        # self.pTime = 0
+        # self.cTime = 0
+        # capture = cv2.VideoCapture(0)
+        # capture.set(3, 1280)
+        # capture.set(4, 720)
+
+    def findHands(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS,
+                                               self.mpDrawingStyle.get_default_hand_landmarks_style(),
+                                               self.mpDrawingStyle.get_default_hand_connections_style())
+        return img
+    def findPosition(self, img, handNo = 0, handL = 8, draw = True):
+        self.lmList = []
+        if self.results.multi_hand_landmarks:
+            myHand = self.results.multi_hand_landmarks[handNo]
+            for id, lm in enumerate(myHand.landmark):
+
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+
+                self.lmList.append([id, cx, cy])
+
+                if draw and id == handL:
+                    cv2.circle(img, (cx, cy), 25, (255,0,255), cv2.FILLED)
+
+        return self.lmList
+
+    def fingersUp(self):
+        fingers = []
+
+        # Thumb
+        if self.lmList[self.tipIds[0]][1] < self.lmList[self.tipIds[0] - 1][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+
+        # Other fingers
+        for id in range(1, 5):
+            if self.lmList[self.tipIds[id]][2] < self.lmList[self.tipIds[id] - 2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+        return fingers
--- main.py
+++ main.py
@@ -1,16 +1,88 @@
-# This is a sample Python script.
+import cv2
+import numpy as np
+import os
+import pygame
+import time
+import HandTrackingModule as htm
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# OpenCV & Mediapipe tracking
+pTime = 0
+cTime = 0
+cap = cv2.VideoCapture(0)
+cap.set(3, 1280)
+cap.set(4, 720)
+detector = htm.handDetector(detectCon=0.85)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# Pygame
+pygame.init()
+background_color = (255,255,255)
+(width, height) = (1280,720)
+screen = pygame.display.set_mode((width,height))
+pygame.display.set_caption("Game")
+screen.fill(background_color)
+pygame.display.flip()
+
+hand = pygame.image.load("assets/1.png")
+hand = pygame.transform.scale(hand, (40, 40))
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+running = True
+
+while running:
+
+    success, img = cap.read()
+    img = cv2.flip(img, 1)
+    img.flags.writeable = False
+    img = detector.findHands(img, draw=False)
+    lmList = detector.findPosition(img)
+    x1, y1 = 0, 0
+    isDrawing = False
+    if len(lmList) != 0:
+        # index finger coord
+        x1, y1 = lmList[8][1:]
+
+        # Middle finger coord
+        # x2, y2 = lmList[12][1:]
+
+        # Return an array [0,0,0,0,0] each element is a finger, 0 is close and 1 is up
+        fingers = detector.fingersUp()
+
+        # 2 finger up (index and middle)
+        if fingers[1] and fingers[2]:
+            print("Select")
+
+        # 1 finger up (index)
+        if fingers[1] and fingers[2] == False:
+            print("Draw")
+            isDrawing = True
+
+        # Hand close
+        if all(finger == False for finger in fingers):
+            print("Close")
+
+    # Show opencv window
+
+    # fps indicator
+    # cTime = time.time()
+    # fps = 1 / (cTime - pTime)
+    # pTime = cTime
+
+    # img.flags.writeable = True
+    # cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+    #
+    # cv2.imshow("Image", img)
+
+    cv2.waitKey(1)
+
+    # Pygame
+    screen.fill(background_color)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    if isDrawing:
+        screen.blit(hand, (x1, y1))
+    pygame.display.update()
+cap.release()
+pygame.quit()
