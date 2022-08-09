878574ca84a6f6db227150b470b4edc6bcaffa7f
s3836387 <62614218+s3836387@users.noreply.github.com>
2022-08-09 16:33:40 +0700

    misc

---
+++ game.py
@@ -0,0 +1,12 @@
+import pygame
+import os
+
+pygame.font.init()
+pygame.mixer.init()
+
+WIDTH, HEIGHT = 900, 500
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("First Game!")
+
+FPS = 60
+VEL = 5
