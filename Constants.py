e1aa874348eca20403d785d96aa71ae7f986f180
s3836387 <62614218+s3836387@users.noreply.github.com>
2022-09-17 13:04:11 +0700

    Update Constants.py

--- Constants.py
+++ Constants.py
@@ -1,6 +1,9 @@
 WIDTH = 1500
 HEIGHT = 800
-UI_STATES = {"main": 0, "levelSelect": 1, "setting": 2, "about": 3, "game": 4, "cleared": 5, "restart": 6, "lose": 7, "next": 8}
+
+UI_STATES = {"main": 0, "levelSelect": 1, "setting": 2, "about": 3,
+             "game": 4, "cleared": 5, "restart": 6, "lose": 7, "next": 8}
+
 COLLISION_TYPES = {"ball": 1, "goal": 2, "border": 3, "die": 4, "line": 5}
 LEVELS_SCORE = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 DELAY = 300
