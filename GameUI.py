e37fa065069ad412db426faf2e99eda3a79cf736
KultrasHub <68211943+KultrasHub@users.noreply.github.com>
2022-09-18 02:53:15 +0700

    Add team profile

--- GameUI.py
+++ GameUI.py
@@ -191,31 +191,31 @@ class aboutUI:
         # --------------------------- END -------------------------------
 
         # ------------------------------- AVATARS -------------------------------
-        avatarSample1 = pygame.image.load('assets/MilkTeaImages/Avatar_Beereel.png').convert_alpha()
-        avatarSample2 = pygame.image.load('assets/MilkTeaImages/Avatar_Princess.png').convert_alpha()
-        avatarSample3 = pygame.image.load('assets/MilkTeaImages/Avatar_Tinia.png').convert_alpha()
-        avatarSample4 = pygame.image.load('assets/MilkTeaImages/Avatar_Scyn.png').convert_alpha()
+        avatarSample1 = pygame.image.load('assets/TeamProfile/Khoa.png').convert_alpha()
+        avatarSample2 = pygame.image.load('assets/TeamProfile/Khai.png').convert_alpha()
+        avatarSample3 = pygame.image.load('assets/TeamProfile/Thang.png').convert_alpha()
+        avatarSample4 = pygame.image.load('assets/TeamProfile/Duc.png').convert_alpha()
 
         khoaX = WIDTH / 2 - 250
         khoaY = HEIGHT / 2 - 50
         self.khoa_Box = AboutUsCompBox(khoaX, khoaY, avatarSample1, "Tran Nguyen Anh Khoa", "s3863956",
-                                       "I worked on the aesthetic, UX/ UI of the game ", 1)
+                                       "I have developed games before, but never a puzzle games. This has been a fun experience to me. ", 1)
 
         khaiX = WIDTH / 2 - 200
         khaiY = HEIGHT / 2 + 100
-        self.khai_Box = AboutUsCompBox(khaiX, khaiY, avatarSample2, "Khai", "s3863956",
-                                       "I worked on the physics simulation",
+        self.khai_Box = AboutUsCompBox(khaiX, khaiY, avatarSample2, "Ngo Quang Khai", "s3836387",
+                                       "I have some experience in game development and very passionate about computer vision technology. ",
                                        1)
 
         thangX = WIDTH / 2 - 70
         thangY = HEIGHT / 2 + 250
-        self.thang_Box = AboutUsCompBox(thangX, thangY, avatarSample3, "Thang", "s3863956",
-                                        "I worked in the computer vision system", 1)
+        self.thang_Box = AboutUsCompBox(thangX, thangY, avatarSample3, "Nguyen Quoc Thang", "s3796613",
+                                        "I found Machine learning and Game Development are interesting topic. Through the project, I have learned valuable skills ", 1)
 
         ducX = WIDTH / 2 + 250
         ducY = HEIGHT / 2 - 150
-        self.duc_Box = AboutUsCompBox(ducX, ducY, avatarSample4, "Duc", "s3863956",
-                                      "I worked in the computer vision system",
+        self.duc_Box = AboutUsCompBox(ducX, ducY, avatarSample4, "Nguyen Huu Duc", "s3669698",
+                                      "I have experience in many computer vision projects and an interest in AI technology. I am always looking for new things and trying to solve problems. ",
                                       1)
 
         # ----------------------------------- END ----------------------------------------
